b8bc7d66ac6c9edaf51bdb44e442a981b69dc356
Pankaj_3387 <pankaj.tayade@embarkingonvoyage.com>
2022-07-04 15:58:22 +0530

    cssLibraray

--- src/app/modules/my-library/my-library.module.ts
+++ src/app/modules/my-library/my-library.module.ts
@@ -7,6 +7,7 @@ import { MyLibraryComponent } from './my-library/my-library.component';
 import { RatingModule } from 'ng-starrating';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ComponentsModule  } from '../../shared/components/components.module'
+import { AngularMaterialModule } from 'src/app/angularMaterialComponents/angular-material/angular-material.module';
 
 
 @NgModule({
@@ -17,7 +18,7 @@ import { ComponentsModule  } from '../../shared/components/components.module'
     CommonModule,
     MyLibraryRoutingModule,
     RatingModule,
-
+    AngularMaterialModule,
     FormsModule,
     ReactiveFormsModule,
     ComponentsModule
